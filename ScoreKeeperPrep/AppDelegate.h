36af2d57a7a52950dcb4bcba642711645ab1595b
Yuan zhou <yuanzhou@yuans-air.wlan.rose-hulman.edu>
2018-03-20 12:05:01 -0400

    Initial Commit

---
+++ ScoreKeeperPrep/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScoreKeeperPrep
+//
+//  Created by Yuan zhou on 3/20/18.
+//  Copyright © 2018 Yuan zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScoreKeeperPrep/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScoreKeeperPrep
+//
+//  Created by Yuan zhou on 3/20/18.
+//  Copyright © 2018 Yuan zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
